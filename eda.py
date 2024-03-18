104f8ef5a96ac5aff72ca1c0ecbc0a8c7155d972
Admin <gablinavera@gmail.com>
2024-03-18 15:54:11 +0300

    # update

--- eda.py
+++ eda.py
@@ -5,5 +5,16 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 abalones=pd.read_csv('/Users/Admin/Downloads/abalone (1).csv')
-print(abalones)
+# print(abalones)
+
+# abalones.info()
+
+print(abalones.duplicated().sum())
+
+# to get abalone's age - add 1.5 to rings
+
+abalones['Rings'] =  abalones['Rings'] + 1.5
+print(abalones.head(5))
+
+
 
