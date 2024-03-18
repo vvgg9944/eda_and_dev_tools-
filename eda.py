634419e30cc0ff7908edff52b981b5820695f7f4
Admin <gablinavera@gmail.com>
2024-03-18 15:30:05 +0300

    # update

--- eda.py
+++ eda.py
@@ -1,1 +1,9 @@
-print('hi')
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+abalones=pd.read_csv('/Users/Admin/Downloads/abalone (1).csv')
+print(abalones)
+
